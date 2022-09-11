5b8366497104ef5f0dfc3bd926f509b17c9abdbc
perkzen <perko.domen@gmail.com>
2022-09-11 18:11:37 +0200

    feat: image loader

--- src/components/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -13,7 +13,7 @@ const LoadingSpinner: FC<LoadingSpinnerProps> = ({
 }) => {
   return (
     <svg
-      className={`animate-spin -ml-1 mr-3 h-${height} w-${width} text-${color}`}
+      className={`animate-spin h-${height} w-${width} text-${color}`}
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
--- src/components/Menu/RightMenu.tsx
+++ src/components/Menu/RightMenu.tsx
@@ -1,5 +1,5 @@
-import React, { FC } from 'react';
-import Image from 'next/image';
+import React, { FC, useState } from 'react';
+import Image, { ImageLoader } from 'next/image';
 import { trpc } from '../../utils/trpc';
 import UpcomingPaymentsList from '../UpcomingPayment/UpcomingPaymentsList';
 import { BiLogOut } from 'react-icons/bi';
@@ -13,8 +13,13 @@ import {
 import { ModalType } from '../../types/modal';
 
 const RightMenu: FC = () => {
-  const { data, isLoading } = trpc.useQuery(['account.get-user']);
+  const { data } = trpc.useQuery(['account.get-user']);
   const dispatch = useModalDispatch();
+  const [isImageLoading, setIsImageLoading] = useState(true);
+
+  const stopLoadingImage = () => {
+    setIsImageLoading((prev) => !prev);
+  };
 
   const handleSignOut = async () => {
     await signOut();
@@ -30,20 +35,16 @@ const RightMenu: FC = () => {
   return (
     <div className={'w-1/4 bg-secondary h-screen sticky top-0'}>
       <div className={'flex flex-col justify-center items-center mt-20 px-6'}>
+        {isImageLoading && <LoadingSpinner width={'8'} height={'8'} />}
         {typeof data?.image === 'string' && (
-          <>
-            {isLoading ? (
-              <LoadingSpinner width={'8'} height={'8'} />
-            ) : (
-              <Image
-                src={data.image}
-                width={72}
-                height={72}
-                alt={'User'}
-                className={'rounded-full'}
-              />
-            )}
-          </>
+          <Image
+            src={data.image}
+            width={72}
+            height={72}
+            alt={'User'}
+            className={'rounded-full'}
+            onLoadingComplete={stopLoadingImage}
+          />
         )}
         <h1 className={'font-bold text-lg my-3'}>{data?.name}</h1>
         <div className={'flex flex-row gap-5'}>
