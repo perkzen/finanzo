add52eaae1d535e0f9ec75da14232d6e21f9d6cf
perkzen <perko.domen@gmail.com>
2022-09-20 14:22:56 +0200

    feat: month page

--- src/pages/report/[year]/[month]/index.tsx
+++ src/pages/report/[year]/[month]/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import Title from '../../../../components/Title/Title';
 
 const Index = () => {
   const router = useRouter();
+  const { year, month } = router.query;
   return (
-    <div>
-      {router.query.year} {router.query.month}
+    <div className={'py-5 mt-10 px-20 flex flex-col w-5/6 gap-8 w-full'}>
+      <Title
+        title={month + ' ' + year}
+        subtitle={'Transactions of selected month'}
+      />
     </div>
   );
 };
