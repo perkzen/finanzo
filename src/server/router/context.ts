03b66c5f494b8a89533b148fe34f23c6c62263a6
perkzen <perko.domen@gmail.com>
2022-10-06 21:00:59 +0200

    feat: middleware

--- src/server/router/context.ts
+++ src/server/router/context.ts
@@ -1,11 +1,9 @@
 import * as trpc from '@trpc/server';
 import * as trpcNext from '@trpc/server/adapters/next';
 import { getSession } from 'next-auth/react';
-import { TRPCError } from '@trpc/server';
 
 export async function createContext(opts?: trpcNext.CreateNextContextOptions) {
   const session = await getSession({ req: opts?.req });
-  if (!session) throw new TRPCError({ code: 'UNAUTHORIZED' });
   return { session, req: opts?.req };
 }
 
