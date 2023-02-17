b09c35042c474330892aa7a89e5840997d59fbe3
perkzen <perko.domen@gmail.com>
2023-02-17 17:29:18 +0100

    fix: remove hello route

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,26 +1,14 @@
-import { publicProcedure, router } from '../trpc';
+import { router } from '../trpc';
 import { accountRouter } from './accounts';
 import { analyticsRouter } from './analytics';
 import { reportsRouter } from './reports';
 import { transactionsRouter } from './transactions';
-import { z } from 'zod';
 
 export const appRouter = router({
   accounts: accountRouter,
   analytics: analyticsRouter,
   reports: reportsRouter,
   transactions: transactionsRouter,
-  hello: publicProcedure
-    .input(
-      z.object({
-        text: z.string(),
-      })
-    )
-    .query(({ input }) => {
-      return {
-        greeting: `hello ${input.text}`,
-      };
-    }),
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
